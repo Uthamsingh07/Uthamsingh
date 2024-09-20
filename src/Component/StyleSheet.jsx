7c2c1d1a390ed30adbb93e319758f2a4bbb0fc17
Uthamsingh07 <uthamsingh98@gmail.com>
2024-09-20 08:50:47 +0530

    SU

---
+++ src/Component/StyleSheet.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import "./Style.css";
+
+
+function StyleSheet() {
+
+   return (
+
+
+
+      <>
+
+         <div className="a">
+
+            <div >
+
+               <h3 className="b"><img className="do" src="blog.img.jpeg" alt="blooger image" width="50px" height="50px" />Blooger</h3>
+               <h6 className="c">Sign IN</h6><br /><br />
+
+               </div>
+
+               <div className="d">
+                  <h1 className="dia">Publish your passions, your way</h1><br />
+                  <h4>Create a unique and beautiful blog easily.</h4>
+                  <br /><br />
+                  <button className="e"><a className="f" href="https://accounts.google.com/AccountChooser/signinchooser?hl=en-GB&service=blogger&ltmpl=blogger&continue=https%3A%2F%2Fwww.blogger.com%2Fhome%23create&ddm=0&flowName=GlifWebSignIn&flowEntry=AccountChooser">Create your blog</a></button>
+               </div>
+             
+
+         </div>
+         <div  className="g">
+            <h1 className="dia" >Choose the perfect design</h1><br /><br />
+            <h6>Create a beautiful blog that fits your style. Choose from a selection <br /><br />of easy-to-use templates – all with flexible layouts and hundreds of <br /><br /> background images – or design something new</h6>
+         </div>
+         <div className="h">
+            <h1 className="dia" >Get a domain</h1><br /><br />
+            <h6>Give your blog the perfect home. Get a blogspot.com domain or buy <br /><br /> a custom domain with just a few clicks.</h6>
+         </div>
+         <div className="i" >
+            <h1 className="dia">Earn money</h1>
+            <br /><br />
+            <h6>Get paid for your hard work. Google AdSense can automatically <br /><br />  display relevant targeted ads on your blog so that you can <br /><br /> earn income by posting about your passion.</h6>
+         </div>
+         <div className="k" >
+            <h1 className="dia">Know your audience</h1><br />
+            <p><h5>Find out which posts are a hit with Blogger’s built-in analytics. You’ll <br /> see where your audience is coming from and what they’re interested <br /> in. You can even connect your blog directly to Google Analytics for a <br /> more detailed look.</h5></p>
+         </div>
+         <div className="l">
+            <h1 className="dia">Hang on to your memories</h1><br /><br />
+            <h6>Save the moments that matter. Blogger lets you safely store <br /><br /> thousands of posts, photos and more with Google.</h6>
+         </div>
+         <div className="m">
+            <h1 className="dia">Join millions of others</h1><br /><br />
+            <h5>Whether sharing your expertise, breaking news or <br /> whatever's on your mind, you're in good company on <br /> Blogger. Sign up to discover why millions of people have <br /> published their passions here.</h5><br /><br />
+            <button className="success">CREATE YOUR BLOG</button>
+         </div>
+
+
+         <div className="n">
+            <div className="o">
+            <div >
+               <ul >
+                  <h5>Help</h5>
+                  <li className="die"><a  href="Help Center"></a>Help Center</li>
+                  <li className="die"><a   href="Help Forum"></a>Help Forum</li>
+                  <li className="die"><a  href="Video Tutorials"></a>Video Tutorials</li>
+               </ul>
+            </div>
+            <div>
+               <ul>
+                  <h5>Community</h5>
+                  <li className="die"><a className="r" href="Blogger Buzz">Blogger Buzz</a></li>
+
+               </ul>
+            </div>
+            
+
+            <div>
+               
+               <ul  >
+                  <h5>Developers</h5>
+                  <li className="die"><a  href="Blogger API"></a>Blogger API</li>
+                  <li className="die"><a  href="Developer Forum"></a>Developer Forum</li>
+
+               </ul>
+            </div>
+            </div>
+
+
+            <br /><br />
+            <hr />
+            
+       
+
+
+
+
+         <div className="p" >
+
+
+            <a className="q" href="https://policies.google.com/terms?hl=en-GB">Terms of Service</a> | <a className="q" href="">Privacy</a> | <a className="q" href="">Content Policy</a>
+
+
+            <button >
+               <form>
+                  <select >
+                     <option value="Tamil"> Tamil</option>
+                     <option value="Russian"> Russian</option>
+                     <option value="italian"> italian</option>
+                     <option value="Chinese"> Chinese</option>
+                     <option value="Urdu"> Urdu</option>
+                  </select>
+
+               </form>
+            </button>
+
+         </div>
+         </div>
+         
+
+
+
+
+
+
+
+
+
+
+      </>
+   )
+
+
+}
+export default StyleSheet
